7f18d2d8a592a115a3096a7fca9746c54ef2c561
谭瀚涛 <littlehans8@gmail.com>
2017-04-19 16:23:51 +0800

    add local language

---
+++ resources/lang/zh-CN/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '密码必须至少为六个字符，并与之相符。',
+    'reset' => '您的密码已经重置！',
+    'sent' => '密码重置的链接已经发送到您的邮箱，请注意查看，如没有，请到垃圾箱查看！',
+    'token' => '重置密码链接失效，请重新重置密码',
+    'user' => "该邮箱没有对应的用户！",
+
+];
