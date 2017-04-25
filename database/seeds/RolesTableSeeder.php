82d4e7065101c052d1d0d274becbb41697f76f06
谭瀚涛 <littlehans8@gmail.com>
2017-04-25 22:24:29 +0800

    add Role/Role Seeder

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Role;
+use Illuminate\Database\Seeder;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $this->saveRoles('super_admin', '超级管理员', '最高权限的管理员');
+        $this->saveRoles('admin', '普通管理员', '普通权限的管理员，具体权限待定');
+        $this->saveRoles('agent', '代理商', '代理商');
+        $this->saveRoles('vip3', 'vip3', 'vip，大流量版套餐');
+        $this->saveRoles('vip2', 'vip2', 'vip，中流量版套餐');
+        $this->saveRoles('vip1', 'vip1', 'vip，小流量版套餐');
+        $this->saveRoles('free', '免费用户', '免费用户，流量有限，限速');
+    }
+
+    private function saveRoles($name, $display_name, $description)
+    {
+        $role = new Role();
+        $role->name = $name;
+        $role->display_name = $display_name;
+        $role->description = $description;
+        $role->save();
+    }
+
+}
