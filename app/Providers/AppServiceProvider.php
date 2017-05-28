2ccd1fa4bfd8133d529e5a7e942680315b059eca
Littlehans8 <littlehans8@gmail.com>
2017-05-28 17:26:41 +0800

    fix problem/mysql -v > 5.77

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // fix use mysql > 5.77 https://github.com/laravel/docs/blob/5.4/migrations.md#index-lengths--mysql--mariadb
+        Schema::defaultStringLength(191);
     }
 
     /**
