41f9b060d5897f65a7355cf74731d46fdbf32bab
Littlehans8 <littlehans8@gmail.com>
2017-06-16 19:00:34 +0800

    update

--- resources/views/dashboard/person.blade.php
+++ resources/views/dashboard/person.blade.php
@@ -76,7 +76,7 @@
                             <i class="icon-bulb"></i>公告
                         </div>
                         <div class="card-block">
-                            <p>期间所有套餐均已优惠:-)</p>
+                            <p style="color: red">现在购买套餐，买多久送多久，购买后请联系管理员帮你延长使用时间:-)</p>
                             <p>有任何问题请联系qq哦</p>
                             <a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=3287229704&site=qq&menu=yes"><img border="0" src="http://wpa.qq.com/pa?p=2:3287229704:51" alt="点击这里给我发消息" title="点击这里给我发消息"/></a>
                         </div>
